a8755465c239f4c5d2e7be7beedb09a9e15cff15
scottbarnesg <scottgbarnes@gwu.edu>
2020-06-25 20:38:39 -0400

    Created web service

---
+++ java/web-services-monitor/src/main/java/com/web/services/monitor/WebServicesMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.web.services.monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServicesMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServicesMonitorApplication.class, args);
+	}
+
+}
---
+++ java/web-services-monitor/src/test/java/com/web/services/monitor/WebServicesMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.services.monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServicesMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
